41a4cb7ba98c0457dcd0e0a423aeccff0f4f8cd4
vikram <vsskkv@gmail.com>
2019-04-19 22:38:04 +0100

    login system created and auth made

--- AnimalSanc/resources/views/home.blade.php
+++ AnimalSanc/resources/views/home.blade.php
@@ -1,23 +1,20 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class=”container”> 
+	@if(\Session::has('error')) 
+	<div class=”alert alert-danger”> {{\Session::get('error')}} </div> 
+	@endif <div class=”row”> 
+		<div class=”col-md-8 col-md-offset-2"> 
+			<div class=”panel panel-default”> 
+				<div class=”panel-heading”>Dashboard</div> 
+				<?php if(auth()->user()->isAdmin == 1){?> 
+					<div class=”panel-body”> <a href=”{{url('admin/routes')}}”>Admin</a> </div>
+				<?php } else echo '<div class=”panel-heading”>Normal User</div>';?> 
+			</div> 
+		</div> 
+	</div> 
+</div> 
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
