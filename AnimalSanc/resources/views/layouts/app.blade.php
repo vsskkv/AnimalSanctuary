7a45c1ca8358a03c11ca47110e5b2c4fbf269af9
vikram <vsskkv@gmail.com>
2019-04-20 15:02:46 +0100

    font awesome added

--- AnimalSanc/resources/views/layouts/app.blade.php
+++ AnimalSanc/resources/views/layouts/app.blade.php
@@ -16,6 +16,9 @@
     <!--Bootstrap-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+     {{-- Font Awesome --}}
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
 </head>
 <body>
     <div id="app">
