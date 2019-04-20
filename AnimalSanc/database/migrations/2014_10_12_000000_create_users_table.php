3485402772116b8efd051fd8d26322897246474a
vikram <vsskkv@gmail.com>
2019-04-20 12:23:33 +0100

    Admin seeded into DB

--- AnimalSanc/database/migrations/2014_10_12_000000_create_users_table.php
+++ AnimalSanc/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('isAdmin')->nullable();
+            $table->timestamp('approved_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
