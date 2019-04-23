54197202d76fa74b3892d7ddda20989effa366d6
vikram <vsskkv@gmail.com>
2019-04-23 01:41:50 +0100

    real nav bar added to app.blade.main

--- AnimalSanc/resources/views/layouts/app.blade.php
+++ AnimalSanc/resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <link href="{{ asset('css/Table.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/_Nav.css') }}" rel="stylesheet" type="text/css">
+
 
 
     <!--Bootstrap-->
@@ -23,89 +25,67 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 
 </head>
-<body>
-    <div id="app">
-
-        
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <i class="fas fa-paw"></i>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav">
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                      </li>
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-dark indigo">
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fas fa-paw">AnimalSanc</i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+                          @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li> 
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->first_name }} <span class="caret"></span>
-                                </a>
+                        @endif
+                    @else      
+                        <!-- If theuser is admin do Part 1 ELSE Part 2 -->
+                        <?php if(auth()->user()->isAdmin == 1){?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('users.index') }}"><i class="fas fa-user-cog"></i> Admin Panel</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{ ="nav-link" { route('pets.index') }}">Pets Panel</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('pets.create') }}">Pets Create Panel</a>
+                        </li>
+                            
+                        <?php } ?>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <!-- Logout -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <i class="fas fa-sign-out-alt"></i> Logout 
+                            </a>
+                        </li>
+                    @endguest
+    </ul>
+    <span class="navbar-text white-text">
+      Hi {{ Auth::user()->first_name }} 
+    </span>
+  </div>
+</nav>
+    <section class="home">
 
+    </section>
 
-                                    
-                                    <!-- If theuser is admin do Part 1 ELSE Part 2 -->
-                                    <?php if(auth()->user()->isAdmin == 1){?>
-                                    <div class=”panel-body”>
-                                        <a class="dropdown-item" href="{{ route('users.index') }}"><i class="fas fa-user-cog"></i> Admin Panel</a>
-                                        <a class="dropdown-item" href="{{ route('pets.index') }}">Pets Panel</a>
-                                    </div> <?php } else echo '<div class="dropdown-item">Welcome</div>';?>
-
-
-                                    <!-- Line seperator -->
-                                    
-
-                                    <!-- Logout -->
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        <i class="fas fa-sign-out-alt"></i> Logout 
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+    <div style="height: 1000px">
+        @yield('content')
     </div>
+</div>
+</div>
+</div>
 
     @include('Partials._scripts')
 
